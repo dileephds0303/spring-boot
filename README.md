92e98ebe532a7fda227cd8598a1093bad24f7c7d
dileephds0303 <dileephds0303@gmail.com>
2025-09-08 19:21:06 +0530

    First Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# spring-boot
-Root Repository Created for Spring Boot
--- src/main/java/com/employee/repositories/EmployeeRepo.java
+++
@@ -1,7 +0,0 @@
-package com.employee.repositories;
-
-import com.employee.entities.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepo extends JpaRepository<Employee,Long> {
-}
